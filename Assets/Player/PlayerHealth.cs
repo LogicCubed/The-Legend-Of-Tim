d0433c9ec2e68243905575ba86c10b389975b4aa
UnLogic <johannocuaman@gmail.com>
2025-03-06 11:02:59 -0800

    Tested Github Desktop Responsiveness
    this took way longer than I needed it to

--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
     public int InvincibilitySeconds;
 
     public PlayerShield playerShield;
-
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
