5c37dddfc6bc92cc80608f3e253e53eb9b3137e1
UnLogic <johannocuaman@gmail.com>
2025-03-12 20:33:37 -0700

    Fixed Styling and Color

--- Assets/Environment/FlameTrap/FlameTrap.cs
+++ Assets/Environment/FlameTrap/FlameTrap.cs
@@ -34,7 +34,7 @@ public class FlameTrap : MonoBehaviour
 
     IEnumerator Flame()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.4f);
 
         Instantiate(flame, transform.position + new Vector3(0, 2f, -1f), Quaternion.identity);
     }
