2b65e3bad3180ec20400eb116123b28c840134dc
UnLogic <johannocuaman@gmail.com>
2025-03-12 17:41:48 -0700

    Power Up FX
    Added drop shadow, glint, and bob animation for Shield + Health power-ups

--- Assets/Collectibles/Power-Ups/Health/HealthPowerUp.cs
+++ Assets/Collectibles/Power-Ups/Health/HealthPowerUp.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class HealthPowerUp : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    private float startY;
+    private Transform shadow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startY = transform.position.y;
+        shadow = transform.Find("DropShadow");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Bob();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -31,5 +34,16 @@ public class HealthPowerUp : MonoBehaviour
                 Debug.Log("Health is Full!");
             }
         }
+    }
+
+    public void Bob()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            startY + Mathf.Sin(Time.time * 2f) * 0.25f,
+            transform.position.z
+        );
+
+        shadow.position = new Vector3(shadow.position.x, startY - 0.75f, shadow.position.z);
     }
 }
--- Assets/Collectibles/Power-Ups/Shield/ShieldPowerUp.cs
+++ Assets/Collectibles/Power-Ups/Shield/ShieldPowerUp.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class ShieldPowerUp : MonoBehaviour
 {
     public PlayerShield playerShield;
+    private float startY;
+    private Transform shadow;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startY = transform.position.y;
+        shadow = transform.Find("DropShadow");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Bob();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -22,5 +26,16 @@ public class ShieldPowerUp : MonoBehaviour
             playerShield.AddShield();
             Destroy(gameObject);
         }
+    }
+
+    public void Bob()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            startY + Mathf.Sin(Time.time * 2f) * 0.25f,
+            transform.position.z
+        );
+
+        shadow.position = new Vector3(shadow.position.x, startY - 0.75f, shadow.position.z);
     }
 }
